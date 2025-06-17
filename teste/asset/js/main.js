cafc5fbc6e8575ceb93a83331d02c580e99efe75
fahledu <fahledu@gmail.com>
2025-06-17 14:50:16 -0300

    teste

---
+++ teste/asset/js/main.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const urlXLSX = 'asset/data/data.xlsx';
+
+  fetch(urlXLSX)
+    .then(response => response.arrayBuffer())
+    .then(data => {
+      const workbook = XLSX.read(data, { type: 'array' });
+      const sheet = workbook.Sheets[workbook.SheetNames[0]];
+      const lines = XLSX.utils.sheet_to_json(sheet);
+
+      let html = '';
+
+      lines.forEach(line => {
+
+        const name = line.nome || '';
+        const bio = line.bio || '';
+        const image = line.imagem || '';
+
+        html += `
+          <div class="bloco">
+          <h1>${name}</h1>
+          <img src="asset/img/${image}" alt="${name}">
+          <p>${bio}</p>
+        </div>
+    `
+      })
+      document.body.innerHTML = html
+    });
+
+
+});
